18638649a92e4f81e8d076fc1dd992f33e054e6f
nicola <belchuk2005@mail.ru>
2025-02-24 20:39:50 +0300

    WeatherView test

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from services.weather_api import WeatherApi
 
-# Create your views here.
+class WeatherView(APIView):
+    @staticmethod
+    def get(request):
+        city = request.GET.get('city')
+        response = WeatherApi.get_weather_forecast(city)
+        print(response)
+        # TODO доделать логику
