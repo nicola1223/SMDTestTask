d42ffea9daa99d0f31f71ea048712a2e547fc94f
nicola <belchuk2005@mail.ru>
2025-02-24 20:50:04 +0300

    synchro logic

--- weather/services/weather_api.py
+++ weather/services/weather_api.py
@@ -1,5 +1,5 @@
 import os
-import httpx
+import requests
 from dotenv import load_dotenv
 
 env_path = os.path.join(os.path.dirname(__file__), "../.env")
@@ -8,13 +8,12 @@ if os.path.exists(env_path):
 
 class WeatherApi:
     @staticmethod
-    async def get_weather_forecast(city: str) -> dict:
+    def get_weather_forecast(city: str) -> dict:
         api_key = os.getenv("WEATHER_API_KEY")
         api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-        async with httpx.AsyncClient() as client:
-            response = await client.get(api_url)
+        response = requests.get(api_url)
 
-            if response.status_code == 200:
-                return response.json()
-            else:
-                return {}
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {}
